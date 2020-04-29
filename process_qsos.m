8f0145f7e4d90ef72185375eeefd05979a795433
jibancat <mho026@ucr.edu>
2020-04-29 01:51:00 -0700

    make the occams razor to be adaptive based on catalog snrs since most misfits now from high snr cases

--- process_qsos.m
+++ process_qsos.m
@@ -7,17 +7,10 @@
 % Dec 25, 2019: add Lyman series to the noise variance training
 %   s(z)     = 1 - exp(-effective_optical_depth) + c_0 
 % the mean values of Kim's effective optical depth
-%
-% Apr 28: add occams razor for penalising the missing pixels,
-%   this factor is tuned to affect log likelihood in a range +- 500,
-%   this value could be effective to penalise every likelihoods for zQSO > zCIV
-%   the current implemetation is:
-%     likelihood - occams_factor * (1 - lambda_observed / (max_lambda - min_lambda) )
-%   and occams_factor is a tunable hyperparameter
 prev_tau_0 = 0.0023;
 prev_beta  = 3.65;
 
-occams_factor = 1000;
+occams_factor = 2000;
 
 % load QSO model from training release
 variables_to_load = {'rest_wavelengths', 'mu', 'M'};
@@ -31,6 +24,7 @@ load(sprintf('%s/learned_zqso_only_model_%s_norm_%d-%d',             ...
 catalog = load(sprintf('%s/zqso_only_catalog', processed_directory(release)));
 
 z_qsos = catalog.z_qsos;
+snrs   = catalog.snrs;   % catalogue snrs helps to rescale occam's razor
 
 rng('default');
 sequence = scramble(haltonset(1), 'rr2');
@@ -64,6 +58,7 @@ all_pixel_mask     =     all_pixel_mask(test_ind);
 all_thing_ids      =   catalog.thing_ids(test_ind);
 
 z_qsos = catalog.z_qsos(test_ind);
+snrs   = snrs(test_ind);
 
 num_quasars = numel(z_qsos);
 if exist('qso_ind', 'var') == 0
@@ -103,6 +98,10 @@ for quasar_ind = q_ind_start:num_quasars %quasar list
     tic;
     quasar_num = qso_ind(quasar_ind);
     z_true(quasar_ind)   = z_qsos(quasar_num);
+
+    % rescale the occams_factor
+    this_snr             = snrs(quasar_num);
+    this_occams_factor   = occams_factor * this_snr / nanmedian(snrs);
 
     fprintf('processing quasar %i/%i (z_true = %0.4f) ...', ...
         quasar_ind, num_quasars, z_true(quasar_ind));
@@ -207,7 +206,7 @@ for quasar_ind = q_ind_start:num_quasars %quasar list
         sample_log_priors = 0;
 
         % additional occams razor for penalizing the not enough data points in the window
-        occams = occams_factor * (1 - lambda_observed / (max_lambda - min_lambda) );
+        occams = this_occams_factor * (1 - lambda_observed / (max_lambda - min_lambda) );
 
         sample_log_posteriors(quasar_ind, i) = ...
             log_mvnpdf_low_rank(this_flux, this_mu, this_M, this_noise_variance) + sample_log_priors ...
@@ -225,7 +224,7 @@ for quasar_ind = q_ind_start:num_quasars %quasar list
     [~, I] = nanmax(this_sample_log);
     
     z_map(quasar_ind) = offset_samples_qso(I);                                  %MAP estimate
-
+    
     fprintf(' took %0.3fs.\n', toc);
 
     zdiff = z_map(quasar_ind) - z_qsos(quasar_ind);
@@ -240,7 +239,7 @@ end
 variables_to_save = {'training_release', 'training_set_name', 'offset_samples_qso', 'sample_log_posteriors', ...
      'z_map', 'z_qsos', 'all_thing_ids', 'test_ind', 'z_true'};
 
-filename = sprintf('%s/processed_zqso_only_qsos_%s-%s_%d-%d_%d-%d_oc%d', ...
+filename = sprintf('%s/processed_zqso_only_qsos_%s-%s_%d-%d_%d-%d_oca%d', ...
     processed_directory(release), ...
     test_set_name, optTag, ...
     qso_ind(1), qso_ind(1) + numel(qso_ind), ...
